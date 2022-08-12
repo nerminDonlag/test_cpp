e0d87ef1ef9f8af408071e77a9db4a21d5e9db74
nerminDonlag <nermin.donlag@gmail.com>
2022-08-12 15:34:19 +0200

    Add fizzbuzz

---
+++ fizzbuzz.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  int userNumber;
+  cout << "Please enter number: ";
+  cin >> userNumber;
+
+  cout << userNumber << endl;
+
+  return 0;
+}
